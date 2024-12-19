9415959c2ca0a578190250e38089e42cafec3b0d
angelettecortez <cortezangelette@gmail.com>
2024-12-19 19:44:56 +0800

    Create array sorting.js

---
+++ array sorting.js
@@ -0,0 +1,10 @@
+let mergedArray = numbers.concat(names);
+console.log("Merged array:", mergedArray);
+
+
+let sortedNumbers = numbers.sort((a, b) => b - a);
+console.log("Sorted numbers in reverse:", sortedNumbers);
+
+
+let sortedNames = names.sort();
+console.log("Sorted names alphabetically:", sortedNames);
